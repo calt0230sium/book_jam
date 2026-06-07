48f2f52ed15f557d7e9a2e07c841e23967a3921f
Onys <revel.malo@gmail.com>
2026-06-07 18:52:17 +0200

    Améliorer la preview

--- book-game/scripts/book_zoom.gd
+++ book-game/scripts/book_zoom.gd
@@ -1,27 +1,60 @@
 extends Control
 
-@export var ROT_SPEED = 0.
+@export var ROT_SPEED: float
+@export var MOVE_DURATION: float
 
-@onready var book_mesh:MeshInstance3D = $SubViewportContainer/SubViewport/Node3D/Book
+@onready var book:Node3D = $SubViewportContainer/SubViewport/Node3D/Book
+@onready var book_target_position = book.position
+
+# Cover elements
+@onready var book_front_cover: MeshInstance3D = $SubViewportContainer/SubViewport/Node3D/Book/Covers/FrontCover
+@onready var book_back_cover: MeshInstance3D = $SubViewportContainer/SubViewport/Node3D/Book/Covers/BackCover
+@onready var book_binding: MeshInstance3D = $SubViewportContainer/SubViewport/Node3D/Book/Covers/Binding
+
+
+var book_start_position = Vector3(0, -20, 0)
+var book_move_trans = Tween.TRANS_QUINT
 
 func _ready():
-	disable()
-
-func disable():
 	hide()
 
+func mesh_instance_set_color(mi:MeshInstance3D, color:Color):
+	mi.mesh.material.albedo_color = color
+
 func zoom_on_book(ident:String):
-	book_mesh.rotation = Vector3.ZERO
-	book_mesh.mesh.material.albedo_color = BookRef.get_color(ident)
+	book.rotation = Vector3.ZERO
+	var color = BookRef.get_color(ident)
+	mesh_instance_set_color(book_front_cover, color)
+	mesh_instance_set_color(book_back_cover, color)
+	mesh_instance_set_color(book_binding, color)
 	show()
+	
+	# Make the book rise
+	book.position = book_start_position
+	var move_tween = create_tween()
+	move_tween.set_trans(book_move_trans)
+	move_tween.set_ease(Tween.EASE_OUT)
+	move_tween.tween_property(book, "position", book_target_position, MOVE_DURATION)
+
+
+func disable():
+	
+	# Make the book disappear
+	book.position = book_target_position
+	var move_tween = create_tween()
+	move_tween.set_trans(book_move_trans)
+	move_tween.set_ease(Tween.EASE_IN)
+	move_tween.tween_property(book, "position", book_start_position, MOVE_DURATION)
+	move_tween.tween_callback(hide)
+	
 	
 func _process(delta):
 	if visible:
 		if Input.is_action_pressed("ui_left"):
-			book_mesh.rotate(Vector3(0, 1, 0), -ROT_SPEED * delta)
+			book.rotate(Vector3(0, 1, 0), -ROT_SPEED * delta)
 		elif Input.is_action_pressed("ui_right"):
-			book_mesh.rotate(Vector3(0, 1, 0), ROT_SPEED * delta)
+			book.rotate(Vector3(0, 1, 0), ROT_SPEED * delta)
 		elif Input.is_action_pressed("ui_up"):
-			book_mesh.rotate(Vector3(1, 0, 0), -ROT_SPEED * delta)
+			book.rotate(Vector3(1, 0, 0), -ROT_SPEED * delta)
 		elif Input.is_action_pressed("ui_down"):
-			book_mesh.rotate(Vector3(1, 0, 0), ROT_SPEED * delta)
+			book.rotate(Vector3(1, 0, 0), ROT_SPEED * delta)
